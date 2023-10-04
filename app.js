1d3cd2fb2aa773539675eb62ad97cefc817452cc
Jamienorthman <mikoann88@gmail.com>
2023-10-04 16:36:49 +0200

    default disabled submit button, removes attribute only when making a choice

--- app.js
+++ app.js
@@ -4,13 +4,18 @@ const submitButton = document.querySelector('.submit');
 const rating = document.getElementById('rating');
 const rates = document.querySelectorAll('.number');
 
+submitButton.setAttribute('disabled', 'disabled')
+
 submitButton.addEventListener("click", () => {
     thanksContainer.classList.remove("hidden")
     mainContainer.style.display = "none"
-}) 
+})
+
+
 
 rates.forEach((rate) => {
     rate.addEventListener("click", () => {
         rating.innerHTML = rate.innerHTML
+        submitButton.removeAttribute('disabled')
     })
 })
